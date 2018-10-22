a3347995c088adcbc26b0a48116f42d3ebc66e19
Ilias Bartolini <ilias.bartolini@gmail.com>
2018-10-23 01:48:53 +0200

    Setting runlevels

--- privacy-seed.sh
+++ privacy-seed.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+### BEGIN INIT INFO
+# Provides:          privacy-seed
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start daemon at boot time
+# Description:       Enable service provided by daemon.
+### END INIT INFO
 
 sudo touch /var/log/privacy-seed.log
 sudo chmod 777 /var/log/privacy-seed.log
