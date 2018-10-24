b6699d555661ecac4ed75e960cbc9900338b2ef1
Ilias Bartolini <ilias.bartolini@gmail.com>
2018-10-24 05:26:55 +0200

    Refactoring Sound Mixer

--- python/sound.py
+++ python/sound.py
@@ -5,24 +5,24 @@ import subprocess
 import pygame
 from pprint import pprint
 
-pygame.init()
 
-# heartbeat_sound = pygame.mixer.Sound('media/heartbeat.wav')
-pin_sound = pygame.mixer.Sound('media/pin.wav')
-water_droplet_sound = pygame.mixer.Sound('media/water_droplet.wav')
+class SoundMixer:
 
-# def play_heartbeat():
-#     heartbeat_sound.play(-1)
+    def __init__(self):
+        pygame.init()
+        self.pin_sound = pygame.mixer.Sound('media/pin.wav')
+        self.water_droplet_sound = pygame.mixer.Sound('media/water_droplet.wav')
 
-def play_pin():
-    pin_sound.play(-1)
+    def play_pin(self):
+        self.pin_sound.play(-1)
 
-def play_water_droplet():
-    water_droplet_sound.play(-1)
+    def play_water_droplet(self):
+        self.water_droplet_sound.play(-1)
 
-play_pin()
+sound_mixer = SoundMixer()
+sound_mixer.play_pin()
 time.sleep(.74234)
-play_water_droplet()
+sound_mixer.play_water_droplet()
 time.sleep(.251)
-play_pin()
+sound_mixer.play_pin()
 time.sleep(5)
