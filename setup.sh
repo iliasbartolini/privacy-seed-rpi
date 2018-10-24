b72ae18cb49c844ac457454b4e60ab8cb4241c70
Ilias Bartolini <ilias.bartolini@gmail.com>
2018-10-24 22:51:09 +0200

    fix newline error

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-sudo apt-get install python3-serial python3-pip libsdl-mixer1.2
-libsdl-sound1.2 --yes
+sudo apt-get install python3-serial python3-pip libsdl-mixer1.2 libsdl-sound1.2 --yes
 pip3 install pygame
 
 sudo cp /home/pi/privacy-seed-rpi/privacy-seed.sh /etc/init.d/privacy-seed.sh
