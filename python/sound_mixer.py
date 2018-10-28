e2c176f243631b128991ab7b2109a41e947368ad
Ilias Bartolini <ilias.bartolini@gmail.com>
2018-10-28 02:33:54 +0200

    Change base sound pitch and volume

--- python/sound_mixer.py
+++ python/sound_mixer.py
@@ -14,7 +14,7 @@ class SoundMixer:
         self.water_droplet_sound.set_volume(.8)
         self.heartbeat_sound = pygame.mixer.Sound('media/heartbeat.wav')
         self.base_sound = pygame.mixer.Sound('media/base.ogg')
-        self.base_sound.set_volume(.6)
+        self.base_sound.set_volume(.4)
 
     def play_pin_loop(self):
         self.pin_sound.play(-1)
